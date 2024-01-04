e50db50d69a17bbe96f7bd8fb50e6438a15908f6
SeifBadreldin <151947737+SeifBadreldin@users.noreply.github.com>
2024-01-04 18:55:45 +0200

    FINAL

--- grades.py
+++ grades.py
@@ -20,7 +20,12 @@ def add_grade():
                 if row["ID"] == student_code and row["Course Code"] == course_code:
                     print("This student already has a grade for this course.")
                     return
-    write_csv(filename, data, file_exists)
+    with open(filename, 'a', newline='') as csvfile:
+        fieldnames = data.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(data)
     print("Grade Added Successfully")
 
 def edit_grade():
